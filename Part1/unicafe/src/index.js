755138a63db1a717cb88321ffcd908fdfc412de7
Safia <sa.benm.b@gmail.com>
2019-12-05 12:02:52 +0100

    Add Part1

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -5,41 +5,98 @@ import './index.css';
 
 
 const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
+    <button onClick={onClick}>
+        {text}
+    </button>
 )
 
-const Header = (props) => (
-  <h1> {props.text} </h1>
+const Header = ({ text }) => (
+    <h1> {text} </h1>
 )
+
+const Statistic = ({ text, value }) => (
+
+    <tbody>
+            <tr> 
+                <td> {text}</td>
+                <td> {value}</td>
+            </tr> 
+    </tbody>
+    
+)
+
+
+const Statistics = ({ good, neutral, bad }) => {
+
+    const total = good + neutral + bad
+    const average = (good + neutral * 0 + bad * -1) / (good + bad + neutral)
+    const percentage = 100 * good / (good + neutral + bad) + ' %'
+    return (
+        <div>
+            <Header text='statistics' />
+            <table>
+            <Statistic text='good' value={good} />
+            <Statistic text='neutral' value={neutral} />
+            <Statistic text='bad' value={bad} />
+            <Statistic text='all' value={total} />
+            <Statistic text='average' value={average} />
+            <Statistic text='percentage' value={percentage} />
+            </table>
+      </div>
+    )
+}
+
+const History = (props) => {
+    if (props.AllClicks.length === 0) {
+        return (
+            <div>
+                <Header text='Statistics' />
+                <p> No feedback given </p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Statistics good={props.good} neutral={props.neutral} bad={props.bad} />
+        </div>
+    )
+}
+
 
 
 const App = (props) => {
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [AllClicks, setClicks] = useState([])
 
-  const HandleGood = () => setGood(good + 1)
-  const HandleNeutral = () => setNeutral(neutral + 1)
-  const HandleBad = () => setBad(bad + 1)
+    const HandleGood = () => {
+        setClicks(AllClicks.concat('G'))
+        setGood(good + 1)
+    }
+    const HandleNeutral = () => {
+        setClicks(AllClicks.concat('N'))
+        setNeutral(neutral + 1)
+    }
 
-  return (
-    <div>
-      <Header text='Give feedback' />
-      <Button onClick={HandleGood} text='good' />
-      <Button onClick={HandleNeutral} text='neutral' />
-      <Button onClick={HandleBad} text='bad' />
-      <Header text='statistics' />
-      <p>good : {good}</p>
-      <p>neutral : {neutral}</p>
-      <p>bad : {bad}</p>
+    const HandleBad = () => {
+        setClicks(AllClicks.concat('B'))
+        setBad(bad + 1)
+    }
+    return (
+        <div>
 
-    </div>
+            <Header text='give feedback' />
+            <Button onClick={HandleGood} text='good' />
+            <Button onClick={HandleNeutral} text='neutral' />
+            <Button onClick={HandleBad} text='bad' />
+            <History AllClicks={AllClicks} good={good} neutral={neutral} bad={bad} />
+        </div>
 
 
-  )
+    )
 
 }
 
